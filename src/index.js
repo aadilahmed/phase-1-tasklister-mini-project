46b456a22f5bd4dd63bfe3890128b0575959dc88
Aadil Ahmed <aadilahmed0@gmail.com>
2023-04-10 19:36:29 -0700

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    implementToDo(e.target[0].value);
+    form.reset();
+  });
 });
+
+function implementToDo(todo) {
+  let ul = document.getElementById('tasks');
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  btn.addEventListener('click', (e) => e.target.parentNode.remove());
+  li.appendChild(document.createTextNode(`${todo} `));
+  li.appendChild(btn);
+  ul.appendChild(li);
+}
